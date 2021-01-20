8bf4a847459bb3dcc95ea419fa5aef276c6a8d03
Dainerx <d.oussamabenghorbel@gmail.com>
2021-01-20 20:34:22 +0100

    numerotation

--- monde.h
+++ monde.h
@@ -3,32 +3,47 @@
 
 #include <stdio.h>
 
-typedef enum {Bleu, Rouge} Couleur;
+typedef enum
+{
+    Bleu,
+    Rouge
+} Couleur;
 
-typedef enum {Chateau, Seigneur, Guerrier, Manant, nul} nomPerso;
+typedef enum
+{
+    Chateau,
+    Seigneur,
+    Guerrier,
+    Manant,
+    nul
+} nomPerso;
 
-typedef struct perso {
+typedef struct perso
+{
     nomPerso nom;
     int x, y;
     int xDest, yDest;
     Couleur couleur;
     int tpsProd;
     nomPerso typeProd;
-    struct perso* next,* previous;
-    struct perso* vNext,* vPrevious;
+    struct perso *next, *previous;
+    struct perso *vNext, *vPrevious;
 } Personnage;
 
-typedef struct {
-    Personnage* perso;
+typedef struct
+{
+    Personnage *perso;
 } Case;
 
-typedef struct {
-    Case*** plateau;
-    Personnage* chateauRouge,* chateauBleu;
+typedef struct
+{
+    Case ***plateau;
+    Personnage *chateauRouge, *chateauBleu;
 } Monde;
 
-Monde* initMonde(void);
+Monde *initMonde(void);
 
-void afficheMonde(Monde* monde, int tresorBleu, int tresorRouge, Couleur couleur);
-
+void afficheMonde(Monde *monde, int tresorBleu, int tresorRouge, Couleur couleur);
+void initCounters();
+int incrementAndGet(Couleur color, nomPerso perso);
 #endif
